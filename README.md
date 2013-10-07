5f121008dba512e9bc3db4924e57c2542d453fcc
Takamasa Suzuki <webcrafts098@gmail.com>
2013-10-07 11:42:15 +0900

    rails new bbs

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Ruby初心者勉強会in沖縄 2013/10/08
 
+## 今日の課題
+
+- railsで掲示板をつくろう
+
+## おわりに
+
+株式会社wEVAではryukyu.rbとして、毎週火曜日（月曜日）の19:00〜 RubyやRails、Sinatraの勉強会を沖縄県浦添市で開催しています。興味のある方はぜひグループに参加してイベントに参加よろしくお願いします。
+
+[https://www.facebook.com/groups/ruby.okinawa/](https://www.facebook.com/groups/ruby.okinawa/)
+
+[https://www.facebook.com/wEVA.official](https://www.facebook.com/wEVA.official)
+
---
+++ bbs/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bbs::Application.config.secret_key_base = 'c2dc7fcf5ec5ad85d2b281f6713b263449b8162319545756be573f173819e5164a0da48accdd9399da596f60c6bb04990feb4ca4e85a066e5839d46b12a6d8d6'
